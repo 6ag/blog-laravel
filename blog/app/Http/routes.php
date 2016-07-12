df8dde7519abec369c96ded70675072291b153d7
zhoujianfeng <admin@6ag.cn>
2016-07-12 12:44:37 +0800

    重新配置路由

--- blog/app/Http/routes.php
+++ blog/app/Http/routes.php
@@ -1,64 +1,53 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 // 后台
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function() {
 
     // 无需登录session验证
     Route::group([], function() {
         // 登录
-        Route::any('login', ['as' => 'admin.login', 'uses' => 'LoginController@login']);
+        Route::any('login', 'LoginController@login')->name('admin.login');
 
         // 验证码
-        Route::get('code', ['as' => 'admin.code', 'uses' => 'LoginController@code']);
+        Route::get('code', 'LoginController@code')->name('admin.code');
     });
 
     // 需要登录session验证
     Route::group(['middleware' => ['admin.login']], function() {
         // 后台首页
-        Route::get('index', ['as' => 'admin.index', 'uses' => 'IndexController@index']);
+        Route::get('index', 'IndexController@index')->name('admin.index');
 
         // 网站基本信息
-        Route::get('info', ['as' => 'admin.info', 'uses' => 'IndexController@info']);
+        Route::get('info', 'IndexController@info')->name('admin.info');
 
         // 修改密码
-        Route::any('pass', ['as' => 'admin.pass', 'uses' => 'IndexController@modifyPassword']);
+        Route::any('pass', 'IndexController@modifyPassword')->name('admin.pass');
 
         // 退出登录
-        Route::get('logout', ['as' => 'admin.logout', 'uses' => 'LoginController@logout']);
+        Route::get('logout', 'LoginController@logout')->name('admin.logout');
 
         // 分类管理
         Route::resource('category', 'CategoryController');
-        Route::post('category/changeorder', ['as' => 'admin.category.changeorder', 'uses' => 'CategoryController@changeorder']);
+        Route::post('category/changeorder', 'CategoryController@changeorder')->name('admin.category.changeorder');
 
         // 文章管理
         Route::resource('article', 'ArticleController');
 
         // 上传图片
-        Route::any('upload', ['as' => 'admin.upload', 'uses' => 'CommonController@upload']);
+        Route::any('upload', 'CommonController@upload')->name('admin.upload');
 
         // 友情链接
         Route::resource('links', 'LinksController');
-        Route::post('links/changeorder', ['as' => 'admin.links.changeorder', 'uses' => 'LinksController@changeorder']);
+        Route::post('links/changeorder', 'LinksController@changeorder')->name('admin.links.changeorder');
 
         // 导航栏
         Route::resource('navs', 'NavsController');
-        Route::post('navs/changeorder', ['as' => 'admin.navs.changeorder', 'uses' => 'NavsController@changeorder']);
+        Route::post('navs/changeorder', 'NavsController@changeorder')->name('admin.navs.changeorder');
 
         // 网站配置
         Route::resource('config', 'ConfigController');
-        Route::post('config/changeorder', ['as' => 'admin.config.changeorder', 'uses' => 'ConfigController@changeorder']);
-        Route::post('config/changecontent', ['as' => 'admin.config.changecontent', 'uses' => 'ConfigController@changecontent']);
+        Route::post('config/changeorder', 'ConfigController@changeorder')->name('admin.config.changeorder');
+        Route::post('config/changecontent', 'ConfigController@changecontent')->name('admin.config.changecontent');
     });
 });
 
@@ -66,11 +55,11 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function() {
 Route::group(['namespace' => 'Home'], function() {
 
     // 首页
-    Route::get('/', ['as' => 'home.index', 'uses' => 'IndexController@index']);
+    Route::get('/', 'IndexController@index')->name('home.index');
 
     // 分类列表
-    Route::get('cate/{cate_id}', ['as' => 'home.cate', 'uses' => 'IndexController@cate']);
+    Route::get('cate/{cate_id}', 'IndexController@cate')->name('home.cate');
 
     // 内容页
-    Route::get('news/{art_id}', ['as' => 'home.news', 'uses' => 'IndexController@article']);
+    Route::get('news/{art_id}', 'IndexController@article')->name('home.news');
 });
